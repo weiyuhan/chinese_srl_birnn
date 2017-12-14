2230285192150ed0f32b18b6c498f96320fb4ddb
Wei Yuhan <1300012901@pku.edu.cn>
2017-12-14 14:01:03 +0800

    modify generate predfile

--- generatePredFile.py
+++ generatePredFile.py
@@ -8,8 +8,10 @@ golds = [line.split() for line in goldsFile.readlines()]
 outputFile = open('cpbdev.out', 'w')
 for predsline, goldsline in zip(preds, golds):
 	outline = []
+	newPreds = []
 	lastname = ''
 	preflag = ''
+	modify = False
 	for pred, gold in zip(predsline, goldsline):
 		newPred = pred
 		flag, name = pred[:pred.find('-')], pred[pred.find('-')+1:]
@@ -17,12 +19,18 @@ for predsline, goldsline in zip(preds, golds):
 			lastname = name
 		elif flag == 'I' or flag == 'E':
 			if name != lastname:
-				print(predsline)
 				if preflag == '' or preflag == 'O' or preflag == 'S' or preflag == 'E' or preflag == 'rel':
 					newPred = 'B-' + name
 					lastname = name
 				else:
 					newPred = flag + '-' + lastname
+				modify = True
 		preflag = flag
+		newPreds.append(newPred)
 		outline.append(gold + '/' + newPred)
+		if modify:
+			print('--------------------')
+			print(predsline)
+			print(newPreds)
+			print('--------------------')
 	outputFile.write(' '.join(outline) + '\n')
